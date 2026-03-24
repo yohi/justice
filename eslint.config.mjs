b0a306e05b1da042f70fd4c73967bf632bc319de
Yusuke Ohi <y.ohi@diamondhead.tech>
2026-03-24 10:49:43 +0900

    feat: プロジェクト足場の構築 (package.json, tsconfig, vitest, eslint)

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/explicit-function-return-type": "warn",
+    },
+  },
+);
